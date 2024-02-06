da9827948e0d5a96f7befaa2d0017ebb447a8a7a
hnnhcmmngs <hannahcummings94@gmail.com>
2024-02-05 22:02:23 -0500

    Create playRound function
    In each round of rock, paper, scissors, the player's choice and
    the computer's choice need to be compared to determine who wins.
    The function playRound takes the player's choice and the
    computer's choice and returns a string that declars if the
    player won, lost, or tied with the computer.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * 3)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return `You tied! You both chose ${capitalizeFirstLetter(computerSelection)}!`
+    } else if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "paper") {
+            return "You Lost! Paper beats Rock!"
+        } else {
+            return "You Won! Rock beats Scissors!"
+        }
+    } else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "rock") {
+            return "You Won! Paper beats Rock!"
+        } else {
+            return "You Lost! Scissors beats Paper!"
+        }
+    } else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "rock") {
+            return "You Lost! Rock beats Scissors!"
+        } else {
+            return "You Won! Scissors beats Paper!"
+        }
+    } else {
+        return "Please choose either Rock, Paper, or Scissors!"
+    }
+}
+
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
 }
