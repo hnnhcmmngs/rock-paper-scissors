8bb0360037a6310782a571b2e50dad58080fa0de
hnnhcmmngs <hannahcummings94@gmail.com>
2024-02-08 14:43:12 -0500

    Create choice buttons and event listeners
    In order to create a UI for the rock, paper, scissors game, there
    needs to be three buttons each with one of the options, and when
    one of the buttons is clicked a round of the game is played. These
    buttons have been created in index.html and event listeners have
    been attached to each button. When a button is pressed, the
    playRound function is called with the user's choice and a random
    computer choice generated by the getComputerChoice function in
    script.js. The result of the round is printed in the console.

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection) {
+    if (playerSelection === computerSelection) {
         return `You Tied! You both chose ${capitalizeFirstLetter(computerSelection)}!`
-    } else if (playerSelection.toLowerCase() === "rock") {
+    } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             return "You Lost! Paper beats Rock!"
         } else {
             return "You Won! Rock beats Scissors!"
         }
-    } else if (playerSelection.toLowerCase() === "paper") {
+    } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             return "You Won! Paper beats Rock!"
         } else {
             return "You Lost! Scissors beats Paper!"
         }
-    } else if (playerSelection.toLowerCase() === "scissors") {
+    } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             return "You Lost! Rock beats Scissors!"
         } else {
@@ -29,28 +29,25 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function playGame() {
-    let player_score = 0;
-    let computer_score = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Rock, Paper, or Scissors?");
-        let result = playRound(playerChoice, getComputerChoice());
-        console.log(result)
-        if (result.includes("Won!")) {
-            player_score += 1
-        } else if (result.includes("Lost!")) {
-            computer_score += 1
-        }
-    }
-    if (player_score > computer_score) {
-        console.log("The game is over! You Won!")
-    } else if (player_score < computer_score) {
-        console.log("The game is over! You Lost!")
-    } else {
-        console.log("The game is over! You Tied!")
-    }
-}
-
 function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", () => {
+    let result = playRound("rock", getComputerChoice());
+    console.log(result);
+})
+
+paper.addEventListener("click", () => {
+    let result = playRound("paper", getComputerChoice());
+    console.log(result);
+})
+
+scissors.addEventListener("click", () => {
+    let result = playRound("scissors", getComputerChoice());
+    console.log(result);
+})
